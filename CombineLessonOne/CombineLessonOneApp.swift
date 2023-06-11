d97f03da178101b647c13d77463a4082bdeae87b
David Malicke <davemalicke@gmail.com>
2023-06-11 11:02:07 -0400

    Initial Commit

---
+++ CombineLessonOne/CombineLessonOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineLessonOneApp.swift
+//  CombineLessonOne
+//
+//  Created by David Malicke on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineLessonOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
